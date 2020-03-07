e31b24b2db96ecd7243823da8b1178e3233bbe95
Oleg Tarassov <oleg.tarassov@plusgrade.com>
2020-03-07 09:46:07 -0500

    latest.

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,10 +1,10 @@
 def find_anagrams(word, candidates):
 
-    sorted_word = "".join(sorted(word.lower()))
+    sorted_word = sorted(word.lower())
 
     return [
         can_word
         for can_word in candidates
-        if "".join(sorted(can_word.lower())) == sorted_word
+        if sorted(can_word.lower()) == sorted_word
         and can_word.lower() != word.lower()
     ]
