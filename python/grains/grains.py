d949126cd9462645e956943a34a73ec66745718b
Oleg Tarassov <oleg.tarassov@plusgrade.com>
2020-04-12 15:41:06 -0400

    Added alternative solution.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -3,12 +3,14 @@ def square(number: int) -> int:
     if number < 1 or number > 64:
         raise ValueError("Negative number")
 
+    # Readability + Speed
     total = 1
     for num in range(2, number+1):
         total = total * 2
 
     return total
 
+    #return 2**(number-1)
 
 def total() -> int:
 
