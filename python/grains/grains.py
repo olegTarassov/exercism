2338ea77f09fe48af0677e7f42085276f05efc0b
Oleg Tarassov <oleg.tarassov@plusgrade.com>
2020-04-12 15:28:13 -0400

    Added latest.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,19 @@
-def square(number):
-    pass
+def square(number: int) -> int:
+
+    if number < 1 or number > 64:
+        raise ValueError("Negative number")
+
+    total = 1
+    for num in range(2, number+1):
+        total = total * 2
+
+    return total
 
 
-def total():
-    pass
+def total() -> int:
+
+    total = 0
+    for tot in range(1, 65):
+        total += square(tot)
+
+    return total
