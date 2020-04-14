676b631d45ee22e0506b2ed47f9eadf60eafe2f4
Oleg Tarassov <oleg.tarassov@plusgrade.com>
2020-04-13 22:34:17 -0400

    Added latest.

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,21 @@
-def factors(value):
-    pass
+def factors(value: int) -> int:
+    """
+    return list of prime devisors
+    """
+
+    result = list()
+
+    x = 2
+
+    while x * x <= value:
+        print(f"running {x}")
+        if value % x:
+            x += 1
+        else:
+            value = value // x
+            result.append(x)
+
+    if value > 1:
+        result.append(value)
+
+    return result
